b7e58eaadfd3d19c705256a470bf2491a8374f6d
Abhishek Das <ad.sdmcet@gmail.com>
2019-07-29 22:03:17 +0530

    Destructing Assignments

--- app/index.js
+++ app/index.js
@@ -40,6 +40,34 @@ hello();
 greeting();
 
 let Name = "Abhishek Das";
-let greet = "Good Morning `$Name`";
+let balance = 100;
+let greet = `Good Morning ${Name}, Balance in account is ${balance}`;
 
 console.log(greet);
+
+let a = [20,30,40];
+let b = [10,...a,50];
+
+console.log(b);//Spread Operator(...) -->works on string even
+
+function collect(...a){
+    console.log(a);
+}
+
+collect(1,2,3);
+
+//Destructing Assignments
+let z = [4,5,6];
+let [four,five]=z;
+console.log(z[0],z[1]);
+console.log(four,five);
+
+//Arrays
+let animals = ["Simba","Sparow"];
+let [lion,bird]=animals;
+console.log(lion,bird);
+
+//Objects
+let king = {name:"Mufasa",kids:9};
+let {name,kids} = king;
+console.log(name,kids);
